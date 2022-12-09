262701abe6bd89478432a0379fc73fedb671825c
michaelmuriith <mikemuriith@gmail.com>
2022-12-09 07:54:33 +0300

    ddf

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -5,14 +5,14 @@
     of the program.
 */
 
-if (process.argv.length <= 2) {
-  process.stderr.write("Missing argument\n");
+const size = parseInt(process.argv[2], 10);
+
+if (Number.isNaN(size) || process.argv.length <= 2) {
   process.stderr.write("Usage: ./1-print_square.js <size>\n");
   process.stderr.write("Example: ./1-print_square.js 8\n");
   process.exit(1);
 }
 
-const size = parseInt(process.argv[2], 10);
 const row = "#".repeat(size);
 
 for (let i = 0; i < size; i++) {
